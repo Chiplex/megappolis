cdfcb968875de2a52cd51f8317d3cdb3a5dae856
unknown <raaz11chip@gmail.com>
2021-03-18 15:32:49 -0400

    Se agrego los modulos por lo visto tiene futuro

--- Modules/Core/Routes/web.php
+++ Modules/Core/Routes/web.php
@@ -1,7 +1,5 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
-
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -13,11 +11,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
-
-Route::get('users/{id}', function ($id) {
-    return $id;
+Route::prefix('core')->group(function() {
+    Route::get('/index/{hola}', 'CoreController@index');
 });
